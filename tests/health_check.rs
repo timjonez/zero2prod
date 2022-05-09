b30431b879225a6e5a09c17341a86a3431b87999
Tim Jones <timjonez@protonmail.com>
2022-05-09 10:21:27 -0500

    [tests/health_check] adjust logic to use dynamic ports for testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,27 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health-check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind to address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind to address");
 
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
