d37e337cbed8beaeb84f197b369f81231486c39c
Tim Jones <timjonez@protonmail.com>
2022-05-09 10:25:34 -0500

    [src/main, tests/health_check] run formatter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind to default port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to default port");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -16,8 +16,7 @@ async fn health_check_works() {
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind to address");
 
