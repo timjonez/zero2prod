38db540d5b548634283e70d280b420ae407123b1
Tim Jones <timjonez@protonmail.com>
2022-05-11 13:49:24 -0500

    [scripts/init_db] add default env vars and adjust the skip docker logic

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -15,15 +15,28 @@ if ! [ -x "$(command -v sqlx)" ]; then
     exit 1
 fi
 
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-    docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+  fi
+  docker run \
+      -e POSTGRES_USER=${DB_USER} \
+      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+      -e POSTGRES_DB=${DB_NAME} \
+      -p "${DB_PORT}":5432 \
+      -d \
+      --name "postgres_$(date '+%s')" \
+      postgres -N 1000
 fi
 
 # Keep pinging Postgres until it's ready to accept commands
