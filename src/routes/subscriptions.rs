7e348c9c68fe7badf7530f637b3b684172f61bde
Tim Jones <timjonez@protonmail.com>
2022-05-12 11:54:11 -0500

    [src/routes/subscriptions] move endpoints into logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -8,8 +8,27 @@ pub struct SubscribeForm {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(form: web::Form<SubscribeForm>, pool: web::Data<PgPool>) -> HttpResponse {
-    match sqlx::query!(
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            tracing::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[tracing::instrument(name = "Saving new subscriber details in database", skip(form, pool))]
+pub async fn insert_subscriber(pool: &PgPool, form: &SubscribeForm) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name)
         VALUES ($1, $2, $3)
@@ -18,13 +37,11 @@ pub async fn subscribe(form: web::Form<SubscribeForm>, pool: web::Data<PgPool>) 
         form.email,
         form.name
     )
-    .execute(pool.get_ref())
+    .execute(pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            println!("Failed to execute query: {}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
