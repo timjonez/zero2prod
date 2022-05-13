db41c597b15587a01d29abd6e15ee315751fc65d
Tim Jones <timjonez@protonmail.com>
2022-05-13 16:55:58 -0500

    [configuration] adjust to use split env files

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,9 +1,44 @@
 use secrecy::{ExposeSecret, Secret};
 
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is to a valid environment. Use `local` or `production`",
+                other
+            )),
+        }
+    }
+}
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -17,7 +52,17 @@ pub struct DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
-    settings.merge(config::File::with_name("configuration"))?;
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_dir = base_path.join("configuration");
+    settings.merge(config::File::from(configuration_dir.join("base")).required(true))?;
+
+    let environment: Environment = std::env::var("APP_ENV")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENV");
+
+    settings
+        .merge(config::File::from(configuration_dir.join(environment.as_str())).required(true))?;
     settings.try_into()
 }
 
