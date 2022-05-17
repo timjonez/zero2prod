f95c338b7ad1afd5151ae1e363af899a30d115e4
Tim Jones <timjonez@protonmail.com>
2022-05-17 15:48:05 -0500

    [src/domain] adjust fn signature to return a result

--- src/domain.rs
+++ src/domain.rs
@@ -8,16 +8,16 @@ pub struct NewSubscriber {
 pub struct SubscriberName(String);
 
 impl SubscriberName {
-    pub fn parse(s: String) -> SubscriberName {
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
         let is_empty_or_whitespace = s.trim().is_empty();
         let is_too_long = s.graphemes(true).count() > 256;
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contains_forbidden_chars = s.chars().any(|g| forbidden_characters.contains(&g));
     
         if is_empty_or_whitespace || is_too_long || contains_forbidden_chars {
-            panic!("{} is not a valid subscriber name.", s)
+            Err(format!("{} is not a valid subscriber name.", s))
         } else {
-            Self(s)
+            Ok(Self(s))
         }
     }
     pub fn inner_ref(&self) -> &str {
