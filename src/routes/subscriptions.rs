8bd2b8e0d5884cdc5c337a294a16ed1ec05af8c0
Tim Jones <timjonez@protonmail.com>
2022-05-17 11:40:40 -0500

    [src/routes/subscriber] remove old helper and adjust to use compiler to validate

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,7 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
-use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct SubscribeForm {
@@ -18,10 +18,11 @@ pub struct SubscribeForm {
     )
 )]
 pub async fn subscribe(form: web::Form<SubscribeForm>, pool: web::Data<PgPool>) -> HttpResponse {
-    if !is_valid_name(&form.name) {
-        return HttpResponse::BadRequest().finish();
-    }
-    match insert_subscriber(&pool, &form).await {
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name),
+    };
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             tracing::error!("Failed to execute query: {:?}", e);
@@ -30,16 +31,16 @@ pub async fn subscribe(form: web::Form<SubscribeForm>, pool: web::Data<PgPool>) 
     }
 }
 
-#[tracing::instrument(name = "Saving new subscriber details in database", skip(form, pool))]
-pub async fn insert_subscriber(pool: &PgPool, form: &SubscribeForm) -> Result<(), sqlx::Error> {
+#[tracing::instrument(name = "Saving new subscriber details in database", skip(new_subscriber, pool))]
+pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name)
         VALUES ($1, $2, $3)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name
+        new_subscriber.email,
+        new_subscriber.name.inner_ref(),
     )
     .execute(pool)
     .await
@@ -48,13 +49,4 @@ pub async fn insert_subscriber(pool: &PgPool, form: &SubscribeForm) -> Result<()
         e
     })?;
     Ok(())
-}
-
-pub fn is_valid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-    let is_too_long = s.graphemes(true).count() > 256;
-    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
-    let contains_forbidden_chars = s.chars().any(|g| forbidden_characters.contains(&g));
-
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_chars)
 }
