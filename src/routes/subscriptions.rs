a1bbd481f6bcd670b2fc61570055006a59a2386a
Tim Jones <timjonez@protonmail.com>
2022-05-18 09:23:26 -0500

    [src/routes/subscriptions] refactor and move the parsing logic into a helper

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,6 +9,12 @@ pub struct SubscribeForm {
     name: String,
 }
 
+pub fn parse_subscriber(form: SubscribeForm) -> Result<NewSubscriber, String> {
+    let email = SubscriberEmail::parse(form.email)?;
+    let name = SubscriberName::parse(form.name)?;
+    Ok(NewSubscriber { email, name })
+}
+
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(form, pool),
@@ -18,17 +24,9 @@ pub struct SubscribeForm {
     )
 )]
 pub async fn subscribe(form: web::Form<SubscribeForm>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name =  match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber =  match parse_subscriber(form.0) {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber {
-        email,
-        name,
     };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
