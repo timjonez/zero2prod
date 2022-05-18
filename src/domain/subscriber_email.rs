1b781de96459611c6d12b04851770fe25e706b66
Tim Jones <timjonez@protonmail.com>
2022-05-18 09:11:00 -0500

    [src/domain/subscriber_email] add testing for happy case with dynamic emails

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -21,8 +21,20 @@ impl AsRef<str> for SubscriberEmail {
 
 #[cfg(test)]
 mod tests {
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
     use super::SubscriberEmail;
     use claim::assert_err;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
 
     #[test]
     fn email_empty_string_invalid() {
@@ -40,5 +52,10 @@ mod tests {
     fn email_missing_subject_invalid() {
         let email = "@test.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_email_valid(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
