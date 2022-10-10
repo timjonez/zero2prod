7d355513eb5ea6753d1224822a5b7f55e2a8c85f
Tim Jones <timjonez@protonmail.com>
2022-10-09 21:41:39 -0500

    [src/routes] add new route to confirm subs

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
+use crate::routes::confirm;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
@@ -69,6 +70,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health-check", web::get().to(health_check))
             .route("/subscriptions/", web::post().to(subscribe))
+            .route("subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
     })
