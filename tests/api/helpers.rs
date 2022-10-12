8ce5b8c0624db9f42382e908180da3ffa795b969
Tim Jones <timjonez@protonmail.com>
2022-10-11 20:01:30 -0500

    [test/api/helpers] add post to testapp

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -20,6 +20,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 
 pub struct TestApp {
     pub address: String,
+    pub port: u16,
     pub db_pool: PgPool,
     pub email_server: MockServer,
 }
@@ -53,11 +54,12 @@ pub async fn spawn_app() -> TestApp {
     let application = Application::build(config.clone())
         .await
         .expect("Failed to build application");
-    let address = format!("http://127.0.0.1:{}", application.port());
+    let app_port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
 
     TestApp {
-        address: address,
+        address: format!("http://127.0.0.1:{}", app_port),
+        port: app_port,
         db_pool: get_connection_pool(&config.database),
         email_server,
     }
