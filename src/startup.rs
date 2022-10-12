693fcf6770bfe3d73478cb9fbb069fa6f2149e1b
Tim Jones <timjonez@protonmail.com>
2022-10-11 20:00:35 -0500

    [src/startup] add app port to server

--- src/startup.rs
+++ src/startup.rs
@@ -44,7 +44,7 @@ impl Application {
 
         let listener = TcpListener::bind(&address)?;
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, connection_pool, email_client)?;
+        let server = run(listener, connection_pool, email_client, configuration.application.base_url)?;
 
         Ok(Self { port, server })
     }
@@ -58,13 +58,18 @@ impl Application {
     }
 }
 
+pub struct ApplicationBaseUrl(pub String);
+
 pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
+    base_url: String,
 ) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
+
+    let base_url = web::Data::new(ApplicationBaseUrl(base_url));
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
@@ -73,6 +78,7 @@ pub fn run(
             .route("subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
+            .app_data(base_url.clone())
     })
     .listen(listener)?
     .run();
