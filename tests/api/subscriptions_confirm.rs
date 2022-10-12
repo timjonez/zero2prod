7568784c55cf8adb2a6cf1ee8f7fbe09388272b5
Tim Jones <timjonez@protonmail.com>
2022-10-11 20:03:00 -0500

    [tests/api/subscriptions_confirm] add app port to confirm link test

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -38,9 +38,11 @@ async fn confirmation_link_returns_200() {
         links[0].as_str().to_owned()
     };
     let raw_confirmation_link = &get_link(&body["HtmlBody"].as_str().unwrap());
-    let confirmation_link = Url::parse(raw_confirmation_link).unwrap();
+    let mut confirmation_link = Url::parse(raw_confirmation_link).unwrap();
 
     assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+
+    confirmation_link.set_port(Some(app.port)).unwrap();
 
     let response = reqwest::get(confirmation_link).await.unwrap();
 
