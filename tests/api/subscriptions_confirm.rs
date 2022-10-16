c8744447f61793cb7bf8c639838e12b17d0d27b5
Tim Jones <timjonez@protonmail.com>
2022-10-15 19:38:57 -0500

    [tests/api/subscriptions_confirm] add test to check confirmation status after confirm click

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -32,3 +32,34 @@ async fn confirmation_link_returns_200() {
 
     assert_eq!(response.status().as_u16(), 200);
 }
+
+#[tokio::test]
+async fn click_on_confirmation_link_confirms_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=test&email=test%40test.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(&email_request);
+
+    reqwest::get(confirmation_links.html)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    let saved = sqlx::query!("SELECT name, email, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "test@test.com");
+    assert_eq!(saved.name, "test");
+    assert_eq!(saved.status, "confirmed");
+}
